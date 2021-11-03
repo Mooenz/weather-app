db7d8698d796022959e297991433fefa99c7f8f2
mooenz <joss92821@hotmail.com>
2021-11-02 20:17:09 -0500

    agrego utilidad selecciona selectores

---
+++ src/utils/DOMElemtens.js
@@ -0,0 +1,18 @@
+// Select element
+const INPUT_SEARCH = document.querySelector('.input');
+const ICON_SEARCH = document.querySelector('.icon');
+const MAIN_RESULT = document.querySelector('.main__result');
+const MAIN_HISTORY_CONTAINER = document.querySelector(
+  '.main__history-container'
+);
+
+// Create element
+const RESULT = document.createElement('div');
+
+export {
+  INPUT_SEARCH,
+  ICON_SEARCH,
+  MAIN_RESULT,
+  RESULT,
+  MAIN_HISTORY_CONTAINER,
+};
