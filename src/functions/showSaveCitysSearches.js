26364ffc4e589d3cc55798fedefa6ddca39cc382
mooenz <joss92821@hotmail.com>
2021-11-05 21:22:07 -0500

    completo func muestra historial de ciudades

--- src/functions/showSaveCitysSearches.js
+++ src/functions/showSaveCitysSearches.js
@@ -1,15 +1,26 @@
 // Utils
 import { MAIN_HISTORY_CONTAINER, RESULT } from '../utils/DOMElemtens.js';
+import { fetchSearchCity } from '../utils/fetch.js';
 
-// Functions
-import { fetchSearchCity } from "./getDataSearch";
+async function showSaveCitysSearches(Citys) {
+  try {
+    MAIN_HISTORY_CONTAINER.innerHTML = '';
+    Citys.map((city) => {
+      fetchSearchCity(city).then((response) => {
+        const { name, main } = response;
 
-async function showSaveCitysSearches(arrayCitys) {
-  arrayCitys.map((city) => {
-    const CITY = fetchSearchCity()
-  })
-
-  // console.log(MAIN_HISTORY_CONTAINER);
+        return (MAIN_HISTORY_CONTAINER.innerHTML += `
+          <div class="city-search">
+            <h3>${name}</h3>
+            <p>${Math.round(main.temp)}°</p>
+            <p>feels like ${Math.round(main.feels_like)}°</p>
+          </div>
+        `);
+      });
+    });
+  } catch (error) {
+    console.log(`Existe un error al mostrar los datos de las ciudades guardadas: ${error}`);
+  }
 }
 
 export { showSaveCitysSearches };
