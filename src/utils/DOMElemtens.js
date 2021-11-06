8527f0b2917b0b374f4a45360550b207cc467c43
mooenz <joss92821@hotmail.com>
2021-11-05 21:21:25 -0500

    agrego dos nuevos elementos del dom

--- src/utils/DOMElemtens.js
+++ src/utils/DOMElemtens.js
@@ -1,10 +1,12 @@
 // Select element
 const INPUT_SEARCH = document.querySelector('.input');
 const ICON_SEARCH = document.querySelector('.icon');
-const MAIN_RESULT = document.querySelector('.main__result');
 const MAIN_HISTORY_CONTAINER = document.querySelector(
   '.main__history-container'
 );
+const MAIN_RESULT = document.querySelector('.main__result-content');
+const MAIN_RESULT_HOURLY = document.querySelector('.main__result-hourly-content');
+const MAIN_RESULT_DAILY = document.querySelector('.main__result-daily-content');
 
 // Create element
 const RESULT = document.createElement('div');
@@ -13,6 +15,8 @@ export {
   INPUT_SEARCH,
   ICON_SEARCH,
   MAIN_RESULT,
-  RESULT,
   MAIN_HISTORY_CONTAINER,
+  RESULT,
+  MAIN_RESULT_HOURLY,
+  MAIN_RESULT_DAILY,
 };
