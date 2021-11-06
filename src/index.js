c391607b1d9ce50fd6bd469956e745557e3e8f8a
mooenz <joss92821@hotmail.com>
2021-11-05 21:25:46 -0500

    ajusto func, importo nuevas func

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import { INPUT_SEARCH, ICON_SEARCH } from './utils/DOMElemtens.js';
 
 // Funtions
 import { getDataSearch } from './functions/getDataSearch.js';
-import { showCurrent } from './functions/showCurrentData.js';
+import { showCurrent } from './functions/showDataCurrent.js';
 import { citySearch } from './functions/saveCitysSearches.js';
 import { showSaveCitysSearches } from './functions/showSaveCitysSearches.js';
+import { showDaily } from './functions/showDataDaily.js';
+import { showHourly } from './functions/showDataHourly.js';
 
 async function searchCity(event) {
   if (event.keyCode === 13 || event.type === 'click') {
     const dataCity = await getDataSearch();
-    showCurrent(dataCity);
-    showSaveCitysSearches(citySearch);
+    const { city, current, hourly, daily } = dataCity;
+    showCurrent({ city, current });
+    showDaily(daily);
+    showHourly(hourly);
   }
 }
+
+showSaveCitysSearches(citySearch);
 
 ICON_SEARCH.addEventListener('click', searchCity, false);
 INPUT_SEARCH.addEventListener('keyup', searchCity, false);
