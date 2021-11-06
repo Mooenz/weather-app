50364c4bba0795d9719ffdf5357fd485c7a073fb
mooenz <joss92821@hotmail.com>
2021-11-05 21:24:44 -0500

    ajusto func, completo el retorno de la hora con 0

--- src/functions/convertTime.js
+++ src/functions/convertTime.js
@@ -2,7 +2,7 @@ function convertTime(timeStamp) {
   const date = new Date(timeStamp * 1000);
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  const formattedTime = `${hours}:${minutes}`;
+  const formattedTime = `${hours}:${minutes}0`;
 
   return formattedTime;
 }
