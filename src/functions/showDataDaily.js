90d347027373f0d6b9538f7c50a4c1c297ea95d3
mooenz <joss92821@hotmail.com>
2021-11-08 16:10:31 -0500

    muestra clima en los proximos dias

--- src/functions/showDataDaily.js
+++ src/functions/showDataDaily.js
@@ -2,20 +2,22 @@
 import { MAIN_RESULT_DAILY } from '../utils/DOMElemtens.js';
 
 // Functions
-import { convertTime } from "./convertTime.js";
-import { convertDate } from "./convertDate.js";
+import { convertDate } from './convertDate.js';
 
 const showDaily = (dailyData) => {
   try {
+    MAIN_RESULT_DAILY.innerHTML = '';
+
     for (const property in dailyData) {
-      const {dt, temp} = dailyData[property];
+      const { dt, temp } = dailyData[property];
+      const { max, min } = temp;
+      const { day, dayNumber, month } = convertDate(dt);
 
-      console.log(convertDate(dt));
+      (MAIN_RESULT_DAILY.innerHTML += `
+        <p>${day}, ${dayNumber} ${month}</p>
+        <p>${Math.round(max)}° / <span>${Math.round(min)}°</span></p>
+      `);
     }
-
-    return MAIN_RESULT_DAILY.innerHTML = `
-
-    `;
   } catch (error) {
     console.error(`Existe un error al mostrar los datos por dias: ${error}`);
   }
