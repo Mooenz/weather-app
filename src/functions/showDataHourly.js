37cf5b5d707cab163dd887c78b34820d36e239d3
mooenz <joss92821@hotmail.com>
2021-11-08 16:16:29 -0500

    muestra el clima en los proximos 24 horas

--- src/functions/showDataHourly.js
+++ src/functions/showDataHourly.js
@@ -2,17 +2,20 @@
 import { MAIN_RESULT_HOURLY } from '../utils/DOMElemtens.js';
 
 // Functions
-import { convertTime } from "./convertTime.js";
+import { convertTime } from './convertTime.js';
 
 const showHourly = (hourlyData) => {
   try {
-    for (const property in hourlyData) {
-      const {dt, temp, weather} = hourlyData[property];
+    MAIN_RESULT_HOURLY.innerHTML = ``;
 
-      console.log();
-      // return (MAIN_RESULT_HOURLY.innerHTML = `
-    
-      // `);
+    for (const property in hourlyData) {
+      const { dt, temp, weather } = hourlyData[property];
+
+      MAIN_RESULT_HOURLY.innerHTML += `
+      <p>${convertTime(dt)}</p>
+      <div>icono</div>
+      <p>${Math.round(temp)}°</p>
+      `;
     }
   } catch (error) {
     console.error(`Existe un error al mostrar los datos por horas: ${error}`);
