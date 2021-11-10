0ed3514bc454f2e0dfbe41fccec5cee0a649c7c1
mooenz <joss92821@hotmail.com>
2021-11-09 21:34:42 -0500

    elimino elemento inecesario

--- src/functions/showDataCurrent.js
+++ src/functions/showDataCurrent.js
@@ -1,32 +1,30 @@
 // Utils
-import { MAIN_RESULT, RESULT } from '../utils/DOMElements.js';
+import { MAIN_RESULT_CURRENT } from '../utils/DOMElements.js';
 
 // Functions
 import { convertTime } from './convertTime.js';
 
-function showCurrent( { city, current }) {
+function showCurrent({ city, current }) {
   try {
-    RESULT.innerHTML = `
+    MAIN_RESULT_CURRENT.innerHTML = `
       <article class="card">
       <div>
         <h2 class="card__title">${city.name}, ${city.country}</h2>
         <p class="card__temp">${Math.round(current.temp)}°</p>
-        <p class="card__like-temp">Feels like ${Math.round(
+        <p class="card__like-temp">Feels Like ${Math.round(
           current.feels_like
         )}°</p>
       </div>
       <div>
-        <p><b>Sunrise</b>${convertTime(current.sunrise)}</p>
-        <p><b>Sunset</b>${convertTime(current.sunset)}</p>
-        <p><b>Pressure </b>${current.pressure} hPa</p>
-        <p><b>Humidity </b>${current.humidity}%</p>
-        <p><b>UV </b>${current.uvi}</p>
-        <p><b>Wind</b>${current.wind_speed} M/h</p>
+        <p><span>Sunrise </span>${convertTime(current.sunrise)}</p>
+        <p><span>Sunset </span>${convertTime(current.sunset)}</p>
+        <p><span>Pressure </span>${current.pressure} hPa</p>
+        <p><span>Humidity </span>${current.humidity}%</p>
+        <p><span>UV </span>${current.uvi}</p>
+        <p><span>Wind </span>${current.wind_speed} M/h</p>
       </div>
       </article>
     `;
-  
-    MAIN_RESULT.appendChild(RESULT);
   } catch (error) {
     console.error(`Existe un error al mostrar los datos completos: ${error}`);
   }
