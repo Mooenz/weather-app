f3ae402935301a16b8c613b1337f54707bb7cba5
mooenz <joss92821@hotmail.com>
2021-11-09 21:31:28 -0500

    elimino elemento inecesario

--- src/utils/DOMElements.js
+++ src/utils/DOMElements.js
@@ -4,19 +4,15 @@ const ICON_SEARCH = document.querySelector('.icon');
 const MAIN_HISTORY_CONTAINER = document.querySelector(
   '.main__history-container'
 );
-const MAIN_RESULT = document.querySelector('.main__result-content');
+const MAIN_RESULT_CURRENT = document.querySelector('.main__result-current');
 const MAIN_RESULT_HOURLY = document.querySelector('.main__result-hourly-content');
 const MAIN_RESULT_DAILY = document.querySelector('.main__result-daily-content');
-
-// Create element
-const RESULT = document.createElement('div');
 
 export {
   INPUT_SEARCH,
   ICON_SEARCH,
-  MAIN_RESULT,
+  MAIN_RESULT_CURRENT,
   MAIN_HISTORY_CONTAINER,
-  RESULT,
   MAIN_RESULT_HOURLY,
   MAIN_RESULT_DAILY,
 };
