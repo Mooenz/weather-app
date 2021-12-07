420673382fcb51adfb3b1fb50547c6c1c2ab4f35
mooenz <joss92821@hotmail.com>
2021-12-07 17:52:37 -0500

    cambio el formato de salida

--- src/functions/convertTime.js
+++ src/functions/convertTime.js
@@ -2,7 +2,7 @@ function convertTime(timeStamp) {
   const date = new Date(timeStamp * 1000);
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  const formattedTime = `${hours}:${minutes}0`;
+  const formattedTime = `${hours}:${minutes}`;
 
   return formattedTime;
 }
