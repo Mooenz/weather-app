a6903a38d0632db77705c448e613a90d90550242
mooenz <joss92821@hotmail.com>
2021-12-07 17:52:06 -0500

    agrego nueva structura para capturar errores

--- src/index.js
+++ src/index.js
@@ -4,25 +4,48 @@ import { INPUT_SEARCH, ICON_SEARCH } from './utils/DOMElements.js';
 // Functions
 import { getDataSearch } from './functions/getDataSearch.js';
 import { showCurrent } from './functions/showDataCurrent.js';
-// import { showSaveCitiesSearches } from './functions/showSaveCitiesSearches.js';
+import { showSaveCitesSearches } from './functions/showSaveCitesSearches.js';
 import { showDaily } from './functions/showDataDaily.js';
 import { showHourly } from './functions/showDataHourly.js';
 
+// Default Search
+let defaultSearch = {
+  keyCode: 13,
+  type: 'click',
+};
+
 async function searchCity(event) {
-  if (event.keyCode === 13 || event.type === 'click') {
-    const cityInput = INPUT_SEARCH.value || 'ibague';
-    const dataCity = await getDataSearch(cityInput);
+  try {
+    const { keyCode, type } = event;
 
-    const { city, current, hourly, daily } = dataCity;
+    if (keyCode === 13 || type === 'click') {
+      const cityInput = INPUT_SEARCH.value || 'ibague';
+      const dataCity = await getDataSearch(cityInput);
+      console.log(dataCity);
 
-    showCurrent({ city, current });
-    showDaily(daily);
-    showHourly(hourly);
-    INPUT_SEARCH.value = ''
+      if (!(dataCity === 400)) {
+        const { city, current, hourly, daily } = dataCity;
+
+        showSaveCitesSearches({
+          name: city.name,
+          temp: current.temp,
+          feels_like: current.feels_like,
+        });
+
+        showCurrent({ city, current });
+        showDaily(daily);
+        showHourly(hourly);
+
+        INPUT_SEARCH.value = '';
+      } else {
+      }
+    }
+  } catch (error) {
+    console.error(`Error en el fetch, ciudad no encontrada: ${error}`);
   }
 }
 
-// searchCity()
+searchCity(defaultSearch);
 
 ICON_SEARCH.addEventListener('click', searchCity, false);
 INPUT_SEARCH.addEventListener('keyup', searchCity, false);
